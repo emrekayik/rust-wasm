2437cae886e5a982239b350d8ec168f466862ae1
Emre Kayık <emrekayik1905@gmail.com>
2022-06-22 15:29:58 +0300

    rust-wasm

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rust-wasm
  WebAssembly experiment in Rust.
+ My guide is (MDN Rust to WASM)[https://developer.mozilla.org/en-US/docs/WebAssembly/Rust_to_wasm]
---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str){
+    alert(&format!("Hello, {}!", name));
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
